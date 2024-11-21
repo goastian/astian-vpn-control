8b9058eb2ede3f0d5234e99669bf117821b0b7a0
Elvis Yerel Roman <yerel9212@yahoo.es>
2024-11-20 20:00:44 -0500

    Added new wrapper to stablish a connection with vpn-core API

---
+++ app/Wrapper/Core.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Wrapper;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Response;
+
+final class Core
+{
+    /**
+     * Client HTTP
+     * @var Client
+     */
+    public $client;
+
+    public function __construct($endpoint, $port = 8000)
+    {
+        $this->client = new Client([
+            'base_uri' => "{$endpoint}:{$port}",
+            'timeout' => 2.0,
+            'verify' => false,
+            'headers' => [
+                'Authorization' => "Bearer " . request()->cookie(config('passport_connect.ids.jwt_token')),
+            ],
+        ]);
+    }
+
+    /**
+     * Mount wireguard interface
+     * @param mixed $interface_name
+     * @param mixed $address
+     * @param mixed $private_key
+     * @param mixed $out_interface
+     * @param mixed $listen_port
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function mountInterface($interface_name, $address, $private_key, $out_interface, $listen_port = 51820)
+    {
+        try {
+            $response = $this->client->request("POST", "/api/wireguard/mount", [
+                'json' => [
+                    'interface_name' => $interface_name,
+                    'address' => $address,
+                    'private_key' => $private_key,
+                    'out_interface' => $out_interface,
+                    'listen_port' => $listen_port
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 201) {
+                return response(null, 201);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Remove the Wireguard Network Interface
+     * @param mixed $interface_name
+     * @param mixed $net
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function removeInterface($interface_name, $net)
+    {
+        try {
+            $response = $this->client->request("DELETE", "/api/wireguard/umount", [
+                'json' => [
+                    "interface_name" => $interface_name,
+                    "out_interface" => $net
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 201) {
+                return response(null, 201);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Shutdown the Wireguard Network Interface
+     * @param mixed $interface_name
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function shutdownInterface($interface_name)
+    {
+        try {
+            $response = $this->client->request("GET", "/api/wireguard/down/$interface_name");
+
+            if ($response->getStatusCode() == 200) {
+                return response(null, 200);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Start the wireguard network interface
+     * @param mixed $interface_name
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function startInterface($interface_name)
+    {
+        try {
+            $response = $this->client->request("GET", "/api/wireguard/up/$interface_name");
+
+            if ($response->getStatusCode() == 200) {
+                return response(null, 200);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Add new peer in the Wireguard Network Interface
+     * @param mixed $device_name
+     * @param mixed $interface_name
+     * @param mixed $public_key
+     * @param mixed $allowed_ips
+     * @param mixed $preshared_key
+     * @param mixed $persistent_keepalive
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function addPeer($device_name, $interface_name, $public_key, $allowed_ips, $endpoint, $preshared_key, $persistent_keepalive)
+    {
+        try {
+            $response = $this->client->request("POST", "/api/wireguard/peer/add", [
+                'json' => [
+                    "interface_name" => $interface_name,
+                    "public_key" => $public_key,
+                    "allowed_ips" => $allowed_ips,
+                    "preshared_key" => $preshared_key,
+                    "persistent_keepalive" => $persistent_keepalive,
+                    "endpoint" => $endpoint,
+                    "device_name" => $device_name
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 201) {
+                return response(null, 201);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Delete peers from Wireguard Network Interface
+     * @param mixed $interface_name
+     * @param mixed $public_key
+     * @return Response|\Illuminate\Contracts\Routing\ResponseFactory|void
+     */
+    public function deletePeer($interface_name, $public_key)
+    {
+        try {
+            $response = $this->client->request("DELETE", "/api/wireguard/peer/delete", [
+                'json' => [
+                    "interface_name" => $interface_name,
+                    "public_key" => $public_key
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 201) {
+                return response(null, 201);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Get the all interface available on the server
+     * @return mixed|\Illuminate\Http\JsonResponse|void
+     */
+    public function networkInterfaces()
+    {
+        try {
+            $response = $this->client->request("GET", "/api/system/network-interfaces");
+
+            if ($response->getStatusCode() == 200) {
+                $data = json_decode($response->getBody(), true);
+                return response()->json($data, 200);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
