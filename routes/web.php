c1bdaa5d3c98bff9bb767ee9fef81d46742238a8
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-03-03 22:52:20 -0500

    feat(web): add new route to get user data

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\GlobalController;
 use Elyerr\Passport\Connect\Controllers\CodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/redirect', [CodeController::class, 'redirect'])->name('redirect');
 Route::get('/callback', [CodeController::class, 'callback'])->name('callback');
+
+Route::get('/user', [GlobalController::class, 'login']);
+
 Route::get("/{any}", [DashboardController::class, 'dashboard'])->where('any', '^(?!api).*$');
