6f786944fac693834dea08b27694c3748e344fdb
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-03-06 17:30:05 -0500

    fix: updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GlobalController;
-use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Setting\SettingController;
 use Elyerr\Passport\Connect\Controllers\CodeController;
 
-Route::get('/redirect', [CodeController::class, 'redirect'])->name('redirect');
+Route::get('/redirect', [CodeController::class, 'redirect'])->name('login');
 Route::get('/callback', [CodeController::class, 'callback'])->name('callback');
-
 Route::get('/user', [GlobalController::class, 'user']);
+Route::post('/logout', [GlobalController::class, 'logout']);
 
-Route::get("/{any}", [DashboardController::class, 'dashboard'])->where('any', '^(?!api).*$');
+Route::get("/{any}", function () {
+    return view('layouts.app');
+})->where('any', '^(?!api).*$');
