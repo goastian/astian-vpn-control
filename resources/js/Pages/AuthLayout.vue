fc51a4f5db5a4d87b79a8410a6bcc6080b0bb36a
Im-Jota <joseluisromeroarce12@gmail.com>
2025-03-07 16:42:11 -0500

    style: new left sidebar nav design

--- resources/js/Pages/AuthLayout.vue
+++ resources/js/Pages/AuthLayout.vue
@@ -1,8 +1,7 @@
 <template>
-    <q-layout view="hHh lpR lFf">
-        <q-header bordered class="bg-primary text-white">
+    <q-layout view="lHh lpR lFf">
+        <q-header bordered class="header">
             <q-toolbar>
-                <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
                 <q-toolbar-title> {{ $app_name }} </q-toolbar-title>
 
@@ -13,30 +12,47 @@
         </q-header>
 
         <q-drawer
+            class="nav"
             show-if-above
             v-model="leftDrawerOpen"
             side="left"
             behavior="desktop"
-            elevated
+            :elevation="0"
+            :width="90"
         >
-            <q-list
-                bordered
-                padding
-                class="rounded-borders text-primary"
-                v-for="(item, index) in menus"
-                :key="index"
-            >
-                <q-item clickable v-ripple @click="openLink(item.route)">
-                    <q-item-section avatar>
-                        <span class="text-2xl" :class="item.icon"></span>
-                    </q-item-section>
+            <div class="nav-container">
+                <q-list
+                    borderedless
+                    padding
+                    class="rounded-borders nav-list"
+                >
+                    <q-item>
+                        <q-item-section>
+                            <img src="/img/icon.webp" />
+                        </q-item-section>
+                    </q-item>
 
-                    <q-item-section>{{ item.name }}</q-item-section>
-                </q-item>
-            </q-list>
+                    <q-item
+                        v-for="(item, index) in menus"
+                        clickable
+                        v-ripple
+                        @click="openLink(item.route)"
+                        :key="index"
+                        :active="$route.name === item.route"
+                        active-class="active-link"
+                    >
+                        <q-item-section avatar>
+                            <span class="text-2xl" :class="item.icon"></span>
+                        </q-item-section>
+
+                    </q-item>
+
+                    <q-separator />
+                </q-list>
+            </div>
         </q-drawer>
 
-        <q-page-container>
+        <q-page-container class="main">
             <router-view />
         </q-page-container>
     </q-layout>
@@ -98,3 +114,56 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.header {
+    background-color: var(--bg-primary);
+    color: var(--text-color);
+    border: none;
+}
+
+.nav-container {
+    background-color: var(--bg-secondary);
+    border-radius: 1rem;
+    height: 100%;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.04);
+    border: .04rem solid var(--border);
+
+}
+
+.nav-list {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    color: var(--icon);
+}
+
+.q-item {
+    position: relative;
+}
+
+.q-item::before {
+    content: '';
+    width: 0;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: .08rem;
+    background-color: var(--blue);
+    transition: 1s ease;
+}
+
+.active-link {
+    color: var(--blue);
+}
+
+.active-link::before {
+    width: 50%;
+}
+
+.main {
+    background-color: var(--bg-primary);
+}
+</style>
