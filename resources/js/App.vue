a50d7abe82761b3bee1e64924eb7111353c3e806
Im-Jota <joseluisromeroarce12@gmail.com>
2025-03-11 09:58:03 -0500

    refactor: remove provider user state

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -25,39 +25,12 @@ export default {
     mounted() {
         //Do not remove this line
         // this.addPeer();
-        this.getUser();
         this.getData();
     },
 
     methods: {
-        
         getData() {
             this.app_name = document.getElementById("app").dataset.appName;
-        },
-
-        async getUser() {
-            try {
-                const res = await this.$api.get("/user");
-                if (res.status == 200) {
-                    this.user = res.data;
-                }
-            } catch (error) {
-                if (error.response.status == 401) {
-                    this.user = {};
-
-                    let meta = this.$route.meta;
-
-                    if (meta && meta.auth) {
-                        this.$router.push({ name: "welcome" });
-                    }
-                }
-                if (error.response.status == 404) {
-                    this.$notification.error(error.response.data.message);
-                }
-                if (error.response.status == 500) {
-                    this.$notification.error(error.response.data.message);
-                }
-            }
         },
 
         addPeer() {
