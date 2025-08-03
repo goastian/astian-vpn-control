7051f4332419f612aaa30bbcc6ddcfac926968b3
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-08-03 16:56:56 -0500

    Update laravel-scripts.sh

--- docker/laravel-scripts.sh
+++ docker/laravel-scripts.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-set -e  
+set -e
 
 cd /var/www
 
-echo "⚙️ Running system configuration..."
+echo "  Running system configuration..."
 
 cp /root/.env /var/www/.env
 
+php artisan settings:system-start
 chown -R www-data:www-data .
 
 find . -type d -exec chmod 750 {} \;
 find . -type f -exec chmod 640 {} \;
 
-chmod -R 770 storage
-chmod -R 770 bootstrap/cache
-chmod 400 .env
- 
-php artisan settings:system-start
-
 chmod 600 secrets/*.pem
+#chmod -R 770 storage
+#chmod -R 770 bootstrap/cache
+chmod 400 .env
 
 echo "Starting PHP-FPM..."
 php-fpm83 -D || { echo "Failed to start PHP-FPM"; exit 1; }
 
 echo "Starting Nginx..."
-exec nginx -g "daemon off;" 
+exec nginx -g "daemon off;"
